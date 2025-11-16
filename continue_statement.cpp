ea61b64d4dbce303c4c667fac3b21035d5b68ee6
Bhavna shriwas <bhavnashriwas851@gmail.com>
2025-11-16 22:02:43 +0530

    Create continue_statement.cpp
    adding code6

---
+++ continue_statement.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*int main(){
+    int i;
+    for(i=1;i<=5;i++){
+        cout<<i<<endl;
+
+    
+    if(i==1)
+     continue;
+    }
+    return 0;
+}
+output:
+   1
+   2
+   3
+   4
+   5
+   */
+/* int main(){
+    int i;
+    for(i=1;i<=5;i++){
+        if(i==1)
+          continue;
+          cout<<i<<endl;
+    }
+    return 0;
+}
+output:
+2
+3
+4
+5
+*/
